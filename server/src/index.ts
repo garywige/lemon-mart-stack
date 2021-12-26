741e105276720ec6cc365adaa33796113930bf27
Gary Wige <91767400+garywige@users.noreply.github.com>
2021-12-26 14:20:42 -0800

    NPM scripting for docker compose

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 export const message = 'Hello, world!'
-console.log(message)
+//console.log(message)
 // Execute 'npm run build' to build your code
 // Execute 'npm start' to build and run your code
 // Execute 'npm test' to run your tests
